4c40345bcfa4ba6ffcbd5bfa475999e7cb4253d6
Carlos Valencia <peluka613@gmail.com>
2020-10-01 10:26:38 -0500

    Create empty project

---
+++ src/main/java/edu/weather/WeatherDemoApplication.java
@@ -0,0 +1,13 @@
+package edu.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/weather/WeatherDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
